e26e847ef6b94bd745a6589b346fbddf41ead6af
shiro-web <shiro-kwsm-46@docomo.ne.jp>
2024-01-11 21:14:15 +0900

    sidebar追加中

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+import Sidebar from './components/sidebar/Sidebar';
+
+function App() {
+  return( 
+  <div className="App">
+    <Sidebar />
+  </div>);
+}
+
+export default App;
